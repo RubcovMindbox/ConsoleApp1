4f0de54794c3bce1cf6c1ce4b6fc012aeebef728
Roman Rubtsov <w-raven91@mail.ru>
2021-08-08 22:32:44 +0300

    c18

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,38 +7,14 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!2");
-            Console.WriteLine("Hello World!3");
-            Console.WriteLine("Hello World!4");
-            Console.WriteLine("Hello World!5");
-            Console.WriteLine("Hello World!6");
-            Console.WriteLine("Hello World!7");
-            Console.WriteLine("Hello World!7");
-            Console.WriteLine("Hello World!7");
-            Console.WriteLine("Hello World!7");
-            Console.WriteLine("Hello World!7");
-            Console.WriteLine("Hello World!7");
-            Console.WriteLine("Hello World!7");
-            Console.WriteLine("Hello World!7");
-            Console.WriteLine("Hello World!7");
-            Console.WriteLine("Hello World!7");
-            Console.WriteLine("Hello World!7");
-            Console.WriteLine("Hello World!7");
-            Console.WriteLine("Hello World!7");
         }
     }
     class People
     {
         public int Id { get; set; }
-        public int Id2 { get; set; }
-        public int Id3 { get; set; }
-        public int Id4 { get; set; }
-        public int Id5 { get; set; }
-        public int Id6 { get; set; }
 
         public void Test()
         {
-            Console.WriteLine("Hello World!7");
             Console.WriteLine("Hello World!7");
         }
     }
