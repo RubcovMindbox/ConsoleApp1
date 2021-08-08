1f1901961c3a6f20316934dc7b2cfc786fae8d83
Roman Rubtsov <w-raven91@mail.ru>
2021-08-08 19:43:38 +0300

    b2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -20,5 +20,6 @@ namespace ConsoleApp1
         public int Id { get; set; }
         public int Id2 { get; set; }
         public int Id3 { get; set; }
+        public int Id4 { get; set; }
     }
 }
