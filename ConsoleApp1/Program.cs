4cc4a8801139b1ab1f7d9acf5b7937bfecd35030
Roman Rubtsov <w-raven91@mail.ru>
2021-08-08 19:42:00 +0300

    c5

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!2");
             Console.WriteLine("Hello World!3");
             Console.WriteLine("Hello World!4");
+            Console.WriteLine("Hello World!5");
         }
     }
     class People
